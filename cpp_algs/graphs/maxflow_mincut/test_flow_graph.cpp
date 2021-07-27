88e59801be476d49ea1a6d54485080fa895d66b1
vitalir2 <vitalyi100@mail.ru>
2021-07-27 14:49:52 +0300

    Update tests for flow graph

--- cpp_algs/graphs/maxflow_mincut/test_flow_graph.cpp
+++ cpp_algs/graphs/maxflow_mincut/test_flow_graph.cpp
@@ -4,6 +4,14 @@
 
 #include "flow_graph.h"
 
+void myAssert(const std::string& name, const std::string& got, const std::string& expected) {
+  if (got == expected) {
+    std::cerr << name << " is ok" << std::endl;
+  } else {
+    std::cerr << "Error: expected " << expected << ", got: " << got << std::endl;
+  }
+}
+
 void testGraphResidual() {
   int e1_from = 0;
   int e1_to = 1;
@@ -12,11 +20,7 @@ void testGraphResidual() {
   e1.flow = 4;
   os << e1.residualCapacityTo(e1_from) << " " << e1.residualCapacityTo(e1_to);
   std::string result = "4 1";
-  if (os.str() == result) {
-    std::cerr << "Residual capacity is ok" << std::endl;
-  } else {
-    std::cerr << "Error: expected " << result << ", got: " << os.str() << std::endl;
-  }
+  myAssert("Residual capacity", os.str(), result);
 }
 
 void testGraphChangeFlow() {
@@ -27,23 +31,33 @@ void testGraphChangeFlow() {
   e1.changeFlowTo(e1_to, 5);
   os << e1.flow;
   std::string result = "5";
-  if (os.str() == result) {
-    std::cerr << "Flow to is ok" << std::endl;
-  } else {
-    std::cerr << "Error: expected " << result << ", got: " << os.str() << std::endl;
-  }
+  myAssert("Flow to", os.str(), result);
   e1.changeFlowTo(e1_from, 2);
   std::string got = std::to_string(static_cast<int>(e1.flow));
   std::string result_from = "3";
-  if (got == result_from) {
-    std::cerr << "Flow from is ok" << std::endl;
-  } else {
-    std::cerr << "Error: expected " << result << ", got: " << got << std::endl;
-  }
+  myAssert("Flow from", got, result_from);
 }
+
+void testAddAndChangeEdge() {
+  FlowGraph fg(2);
+  fg.addEdge({0, 1, 10});
+  std::string got = std::to_string(static_cast<int>(fg.adj(0)[0].flow));
+  std::string result = "0";
+  int vertex_to = 1;
+  myAssert("Add edge and adjacent: flow", got, result);
+  std::string got_rescap = std::to_string(static_cast<int>(fg.adj(0)[0]
+        .residualCapacityTo(vertex_to)));
+  myAssert("Residual capacity in graph", got_rescap, "10"); 
+  fg.adj(0)[0].changeFlowTo(vertex_to, 5);
+  std::string got_second = std::to_string(static_cast<int>(fg.adj(0)[0].flow));
+  myAssert("Change flow in graph", got_second, "5");
+}
+
+
 
 int main() {
   testGraphResidual();
   testGraphChangeFlow();
+  testAddAndChangeEdge();
   return 0;
 }
